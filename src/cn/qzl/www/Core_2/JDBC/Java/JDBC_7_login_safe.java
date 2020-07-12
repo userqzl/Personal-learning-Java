fb154fcdbdbfb2edbdb9bab59bb7bdd56c3a4bfd
userqz <1879799228@qq.com>
2020-07-12 19:00:11 +0800

    添加JDBC_7_login_safe安全登录案例 使用PreparedStatement实现防sql注入登录案例

--- src/cn/qzl/www/Core_2/JDBC/Java/JDBC_7_login_safe.java
+++ src/cn/qzl/www/Core_2/JDBC/Java/JDBC_7_login_safe.java
@@ -6,20 +6,20 @@ import java.sql.*;
 import java.util.Scanner;
 
 /**
- * @description: 数据库登录案例
+ * @description: 使用PreparedStatement实现防sql注入登录案例
  * @author: qzl
- * @created: 2020/07/12 12:16
+ * @created: 2020/07/12 18:39
  */
 
-public class JDBC_6_login {
+public class JDBC_7_login_safe {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("请输入账号：");
-        String user = in.next();
+        String user = in.nextLine();
         System.out.println("请输入密码：");
-        String pwd = in.next();
+        String pwd = in.nextLine();
 
-        JDBC_6_login login = new JDBC_6_login();
+        JDBC_7_login_safe login = new JDBC_7_login_safe();
         boolean islogin = login.login(user,pwd);
         if(islogin){
             System.out.println("登录成功！");
@@ -30,18 +30,22 @@ public class JDBC_6_login {
     }
     public boolean login (String user,String pwd){
         Connection con = null;
-        Statement state = null;
+        PreparedStatement state = null;
         ResultSet resultSet = null;
         try {
             con = JDBCUtil.getConnect();
-            //获取数据库执行对象
-            state = con.createStatement();
 
             //定义sql语句
-            String sql = "select * from user_login where user = '"+user+"' and password = '"+pwd+"'";
-            //执行sql语句
-            resultSet = state.executeQuery(sql);
+            String sql = "select * from user_login where user = ? and password = ?";
+            //获取数据库执行对象
+            state = con.prepareStatement(sql);
+            //给？赋值
+            state.setString(1,user);
+            state.setString(2,pwd);
+            //执行查询
+            resultSet = state.executeQuery();
             return resultSet.next();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
